980eef3ec07be8b7f7f6e2690efda068e93cb0a1
Shivesh Shandilya <shivesh.shandilya@gmail.com>
2024-03-10 23:40:56 +0530

    dark background function added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import './App.css'
+import "./App.css";
 
 function App() {
+  const onClickHandler = async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id! },
+      func: () => {
+        document.body.style.backgroundColor = "grey";
+      },
+    });
+  };
 
   return (
     <>
-        
+      <button onClick={onClickHandler}>dark Background</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
